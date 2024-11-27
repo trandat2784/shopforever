d8f99d7cbb064a81525c3ff4eeadac53b21786a9
trandat2784 <subnauticaearly@gmail.com>
2024-11-27 09:15:23 +0700

    Your commit message for submodule changes

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <p className="w-full md:w-2/3 text-gray-600">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
+            ever since ssthe 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book.
           </p>
         </div>
